8673bfc35b782d4eddc3a8be5ba6616c3d65d6b2
Chion Tang <sdspeed@sina.com>
2015-09-16 17:00:16 +0800

    Add CDN auto selecting

--- bilibili_accelerator_extesion/background.js
+++ bilibili_accelerator_extesion/background.js
@@ -2,11 +2,18 @@ var enabled = false;
 
 bili_cdn = '36.250.74.110';
 
+var speed_test_result = [];
+var speed_test_completed = true;
+var best_url = '';
+
 var on_headers_received_listener = function(details) {
   if (!enabled) {
     return;
   }
   get_bili_cdn();
+  if (bili_cdn.trim() === '') {
+    return;
+  }
   var headers = details.responseHeaders;
   for (var i=0;i<headers.length;i++) {
     if (headers[i].name === 'Location') {
@@ -18,6 +25,73 @@ var on_headers_received_listener = function(details) {
     }
   }
   return {responseHeaders: headers};
+}
+
+var video_url_listener = function(details) {
+  if (!enabled) {
+    return;
+  }
+  if (!details.url.includes('http://interface.bilibili.com/playurl')) {
+    return;
+  }
+  if (!speed_test_completed) {
+    return;
+  }
+  console.log('Fetching URL list');
+  speed_test_result = [];
+  speed_test_completed = false;
+  url_regex = /<url><\!\[CDATA\[(.+?)\]\]><\/url>/g;
+  var url_count = 0;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", details.url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      while (url_match_result = url_regex.exec(xhr.responseText)) {
+        url = url_match_result[1];
+        url_count++;
+        test_video_speed(url, function(time_count, current_url){
+          speed_test_result.push(time_count);
+          if (Math.min.apply(Math, speed_test_result) == time_count) {
+            best_url = current_url;
+          }
+          console.log('speed_test_result.length = ' + speed_test_result.length);
+          if (speed_test_result.length==url_count) {
+            console.log('Test completed.');
+            speed_test_completed = true;
+          }
+        });
+      }
+      console.log('Total url count = ' + url_count);
+    }
+  }
+  xhr.send();
+}
+
+var last_time_stamp = 0;
+var on_before_request_listener = function(details) {
+  if (!enabled) {
+    return;
+  }
+  if (details.url.includes('http://ws.acgvideo.com') || details.url.includes('http://cc.acgvideo.com') || details.url.includes(bili_cdn)) {
+    return;
+  }
+  if (details.type == 'xmlhttprequest') {
+    return;
+  }
+  console.log('New video connection. type=' + details.type);
+  if (!speed_test_completed) {
+    console.log('Speed test not finished. Cancelling.')
+    return {cancel: true};
+  }
+  if (details.timeStamp - last_time_stamp < 3*1000) {
+    console.log('In-browser redirection detected. Ignoring.')
+    return;
+  } else {
+    last_time_stamp = details.timeStamp;
+  }
+  console.log('Selecting fastest url: ' + best_url);
+  return {redirectUrl: best_url};
+
 }
 
 var start_intercept = function() {
@@ -47,8 +121,52 @@ var save_bili_cdn = function(new_cdn) {
   });
 }
 
+chrome.webRequest.onBeforeRequest.addListener(on_before_request_listener, 
+  {urls: ["http://*.acgvideo.com/*"]},
+  ["blocking"]);
+
 chrome.webRequest.onHeadersReceived.addListener(on_headers_received_listener, 
   {urls: ["http://ws.acgvideo.com/*", "http://cc.acgvideo.com/*"]},
   ["blocking", "responseHeaders"]);
 
+chrome.webRequest.onBeforeRequest.addListener(video_url_listener,
+  {urls: ["http://interface.bilibili.com/*"]},
+  ["blocking"]);
+
 get_bili_cdn();
+
+
+function test_video_speed(url, callback) {
+  console.log('Begin speedtest for ' + url);
+  var time_count = 0;
+  var time_count_interval = setInterval(function(){
+    time_count++;
+    if (time_count > 3*1000) {
+      console.log('url=' + url + '; timeout;');
+      callback(99999999, url);
+      xhr.abort();
+      clearInterval(time_count_interval);
+    }
+  }, 1);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url + '&rand=' + getRandomInt(10000000,99999999) , true);
+  xhr.timeout = 3*1000;
+  xhr.onreadystatechange = function() {
+    if (xhr.response.length >= 512*1024) {
+      xhr.abort();
+      console.log('url=' + url + '; time_count=' + time_count);
+      callback(time_count, url);
+      clearInterval(time_count_interval);
+    }
+  }
+  xhr.ontimeout = function() {
+    console.log('url=' + url + '; timeout;');
+    callback(99999999, url);
+    clearInterval(time_count_interval);
+  }
+  xhr.send();
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
